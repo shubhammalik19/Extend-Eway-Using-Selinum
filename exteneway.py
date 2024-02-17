24fef9e9d6960bf5e596cc14195cbb01c0f17927
Shubham Malik <shubhammalik19@users.noreply.github.com>
2024-02-17 17:39:25 +0530

    Update exteneway.py

--- exteneway.py
+++ exteneway.py
@@ -3,12 +3,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
+import os
+from selenium.common.exceptions import NoSuchElementException
+
 
 class extendEwayBill:
     def __init__(self):
         self.login_url = "https://ewaybillgst.gov.in/Login.aspx"
-        self.txt_username = "your_eway_id"
-        self.txt_password = "your_eway_id_password"
+        self.txt_username = "USERNAME"
+        self.txt_password = "PASSWORD"
+
+        log_file = "log.txt";
+        # CHECK IF LOG FILE EXISTS THEN DELETE IT
+        if os.path.exists(log_file):
+            os.remove(log_file)
         self.list_eway_cum_vehicle = []
         self.driver = webdriver.Firefox()
         # READ ALL EWAY BILLS FROM FILE
@@ -47,12 +55,32 @@ class extendEwayBill:
             eway.send_keys(eway_no)
             #create ctl00_ContentPlaceHolder1_Btn_go
             btn_go = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_Btn_go")
-            btn_go.click()
+            
             sleep(10)
-            rbn_extent_0 = self.driver.find_element(By.ID, "rbn_extent_0")
-            rbn_extent_0.click()
-            sleep(10)
-            ddl_extend_select = Select( self.driver.find_element(By.ID, "ddl_extend") )
+            while True:
+                try:
+                    rbn_extent_0 = self.driver.find_element(By.ID, "rbn_extent_0")
+                    break
+                except NoSuchElementException:
+                    continue
+            
+            while True:
+                try:
+                    rbn_extent_0.click()
+                    print("Radio clicked")
+                    break
+                except:
+                    print("Radio not clicked")
+                    continue
+            
+            
+            while True:
+                try:
+                    ddl_extend_select = Select( self.driver.find_element(By.ID, "ddl_extend") )
+                    break
+                except NoSuchElementException:
+                    continue
+            
             ddl_extend_select.select_by_value('4')
             txtRemarks = self.driver.find_element(By.ID, "txtRemarks")
             txtRemarks.send_keys("Transhipment")
@@ -66,10 +94,16 @@ class extendEwayBill:
             
             txtFromEnteredPinCode = self.driver.find_element(By.ID, "txtFromEnteredPinCode")
             txtFromEnteredPinCode.send_keys(txtFromPincode)
-            sleep(10)
+            
+            while True:
+                try:
+                    ctl00_ContentPlaceHolder1_txtDocNo = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txtDocNo").get_attribute("value")
+                    break
+                except NoSuchElementException:
+                    continue
 
             # GET ctl00_ContentPlaceHolder1_txtDocNo VALUE
-            ctl00_ContentPlaceHolder1_txtDocNo = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txtDocNo").get_attribute("value")
+            
             txtDocDate = self.driver.find_element(By.ID, "txtDocDate").get_attribute("value")
             print(ctl00_ContentPlaceHolder1_txtDocNo)
 
@@ -80,15 +114,47 @@ class extendEwayBill:
             ctl00_ContentPlaceHolder1_txtTransDocNo = self.driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_txtTransDocNo")
             ctl00_ContentPlaceHolder1_txtTransDocNo.send_keys(txtDocDate)
 
-            txtDistance  = self.driver.find_element(By.ID, "txtDistance").get_attribute("value")
-            txtDistance = int(txtDistance) - 20  # convert to int before subtracting
-            txtDistanceVal = str(txtDistance)
-            txtDistance = self.driver.find_element(By.ID, "txtDistance")
-            txtDistance.clear()  # clear the input field before sending new keys
-            txtDistance.send_keys(txtDistanceVal)
 
-            sleep(5)
-            btnsbmt = self.driver.find_element(By.ID, "btnsbmt")
-            btnsbmt.click()
-            self.sleep(100)
+            
+
+            while True:
+                try:
+                    btnsbmt = self.driver.find_element(By.ID, "btnsbmt")
+                    print("btnsbmt found")
+                    break
+                except NoSuchElementException:
+                    print("btnsbmt not found")
+                    continue
+            sleep(2)
+            
+            while True:
+                try:
+                    btnsbmt.click()
+                    print("btnsbmt clicked")
+                    break
+                except:
+                    print("btnsbmt not clicked")
+                    continue
+            # Wait until next page has loaded 
+            sleep(20)
+            while True:
+                try:
+                    current_url = self.driver.current_url
+                    print(current_url)
+                    
+                    if current_url == "https://ewaybillgst.gov.in/BillGeneration/EwbExtension.aspx":
+                        #append eway_no in log.txt with status failed
+                        print("failed")
+                        with open("log.txt", "a") as f:
+                            f.write(eway_no + ",failed\n")
+                    else:
+                        #append eway_no in log.txt with status success
+                        with open("log.txt", "a") as f:
+                            f.write(eway_no + ",success\n")
+                        
+                    break
+                except:
+                    continue
+           
+         
 object = extendEwayBill()
